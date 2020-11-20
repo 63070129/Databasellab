cd7ef52ee3e4ff9b69e502b217195a3f40546e3d
63070019 <73679704+63070019@users.noreply.github.com>
2020-11-20 13:40:47 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$id = $_GET['ID'];
+$id = $_GET['id'];
 $sql = "DELETE FROM guestbooks WHERE id = $id";
 
 if (mysqli_query($conn, $sql)) {
